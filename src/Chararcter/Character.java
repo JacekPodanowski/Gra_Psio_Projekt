230380113d3bf8cde0e4e3c26ab507655ba013ba
Jacek Podanowski <jacpodski@gmail.com>
2022-12-21 21:20:53 +0100

    Poprawy zamysłów i nazdór kondzia

--- src/Chararcter/Character.java
+++ src/Chararcter/Character.java
@@ -1,18 +1,58 @@
 package Chararcter;
 
-import Chararcter.Item.Armor;
-import Chararcter.Item.Weapon;
+import Chararcter.Item.*;
 
 public abstract class Character {
     protected int health;
     protected int strength;
-    protected int agility;
     protected int intelligence;
-    //tu powinny być umiejętności
+    protected int agility;
     protected int gold;
     protected int level;
     private Weapon weapon;
     private Armor armor;
+    private Item[] inventory;
+
+    public Character() {
+        this.health = 100;
+        this.inventory = new Item[5];
+    }
+
+    public Character(int health, int strength, int agility, int intelligence, int gold, int level) { //do gracza
+        this.inventory = new Item[5];
+        this.health = health;
+        this.strength = strength;
+        this.agility = agility;
+        this.intelligence = intelligence;
+        this.gold = gold;
+        this.level = level;
+        //this.weapon = mlotek; //początkowa bron to mlotek
+        //this.armor = null; //początkowy armor to brak
+    }
+
+
+    public Character(int health, int strength, int agility, int intelligence, int gold, int level, Weapon weapon, Armor armor) { // do wroga
+        this.inventory = new Item[5];
+        this.health = health;
+        this.strength = 0;//kurwa kondziu po co te zera tu wpisujesz, 10 min kminiłem co nie działa. (health = health a nie 0)
+        this.agility = 0;//zamieniaj sam to sie nauczysz
+        this.intelligence = 0;
+        this.gold = 0;
+        this.level = 0;
+        this.weapon = weapon;
+        this.armor = armor;
+    }
+
+
+
+
+    //public abstract void death();
+
+
+
+    //zamysł uniku jest taki aby dać go do walki tam sie to będzie liczyć, tak uważam. Z poważaniemm jacek podanowksi
+
+
 
 
     public int getHealth() {
@@ -62,32 +102,5 @@ public abstract class Character {
     public void setLevel(int level) {
         this.level = level;
     }
-
-    public Character(int health, int strength, int agility, int intelligence, int gold, int level, Weapon weapon, Armor armor) {
-        this.health = 0;
-        this.strength = 0;
-        this.agility = 0;
-        this.intelligence = 0;
-        this.gold = 0;
-        this.level = 0;
-        this.weapon = weapon;//zadeklaruj w weapon
-        this.armor = armor;//zadeklaruj w armor
-    }
-
-
-    //tu ma być metoda śmierci
-
-
-    //zamysł uniku jest taki aby liczyć unik co 10 pkt zręcznosci dodawać szanse na unik
-    public static void chanse_to_dodge(int agility){
-        double dodge=0.5;
-
-
-
-    }
-
-
-
-
 
 }
