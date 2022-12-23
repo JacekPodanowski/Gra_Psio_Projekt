733730792acae82ec639c7d26f622cce29761f69
Igor Podgórniak <igopood33@gmail.com>
2022-12-24 00:15:54 +0100

    Empty room

--- src/Game/Event/EmptyRoom.java
+++ src/Game/Event/EmptyRoom.java
@@ -1,10 +1,37 @@
 package Game.Event;
 
 import Chararcter.Player;
+import Game.Game;
 
 public class EmptyRoom implements Event {
     @Override
     public Event event(Player player) {
+        int wybor;
+        do{
+            wybor = Game.askForChoice();
+            System.out.println("Co chcesz zrobić?\n 1. Przejdź do następnego pokoju.\t 2. Odpocznij.\t 3. Użyj przedmiotu.");
+            switch(wybor) {
+                case 1:
+                    System.out.println("Przechodzenie do następnego pokoju.");
+                    //metody które przechodzą do next room
+                    break;
+                case 2:
+                    System.out.println("Odpoczywasz.");
+                    //trzeba zdefiniować odpoczynek
+                    break;
+                case 3:
+                    System.out.println("Jakiego przedmiotu chcesz użyć?");
+                    for(int i = 0; i < player.getInventory().length; i++)
+                        System.out.println(player.getInventory()[i].toString());
+                    int itemNumber = Game.askForChoice();
+                    player.getInventory()[itemNumber].useItem();
+                    System.out.println("Użyto przedmiotu " + player.getInventory()[itemNumber].toString());
+                    break;
+                default:
+                    System.out.println("Niepoprawna opcja!");
+                    break;
+            }
+        }while (wybor != 1);
         return null;
     }
 }
