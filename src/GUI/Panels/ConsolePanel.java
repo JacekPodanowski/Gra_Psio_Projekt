a60afc807698e97f0a62f4791ea1ae884795fdff
Michał Porażko <michal.porazko@gmail.com>
2023-01-14 20:57:02 +0100

    Dodanie konsoli w GUI fight

--- src/GUI/Panels/ConsolePanel.java
+++ src/GUI/Panels/ConsolePanel.java
@@ -1,10 +1,24 @@
 package GUI.Panels;
 
+import BackEnd.Game.Event.Fight;
+
 import javax.swing.*;
 
-public class ConsolePanel extends JPanel {
+public class ConsolePanel extends JPanel implements IConsolePanel {
     private JTextArea console = new JTextArea();
-    public ConsolePanel(){
+    private Fight figth;
+    public ConsolePanel(Fight figth){
+        this.figth = figth;
+        figth.setConsolePanel(this);
+        JScrollPane jScrollPane1 = new JScrollPane();
+        jScrollPane1.setViewportView(console);
+        console.setColumns(20);
+        console.setRows(5);
+        console.setEditable(false);
+    }
 
+    @Override
+    public void setMessage(String message) {
+        console.setText(console.getText()+"\n"+message);
     }
 }
