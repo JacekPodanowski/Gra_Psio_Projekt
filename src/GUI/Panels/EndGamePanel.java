d1070d1258fc865a52040ba0d725a199f42c3e8a
Igor Podgórniak <igopood33@gmail.com>
2023-01-25 21:59:46 +0100

    EndGamePanel

--- src/GUI/Panels/EndGamePanel.java
+++ src/GUI/Panels/EndGamePanel.java
@@ -5,6 +5,7 @@ import Observable.Subject;
 import Observers.Observer;
 
 import javax.swing.*;
+import javax.swing.border.Border;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -26,27 +27,36 @@ public class EndGamePanel extends JPanel implements Subject {
         this.setBackground(Color.BLACK);
 
         JLabel endText1 = new JLabel("Uratowałeś swoje życie i wyszedłeś z Kostki");
-        endText1.setForeground(new Color(236, 168, 60));
-        endText1.setFont(new Font("ButtonFont", Font.BOLD, 30));
+        endText1.setForeground(Color.getHSBColor(0,1,0.5f));
+        endText1.setFont(new Font("Verdana", Font.BOLD, 30));
         endText1.setAlignmentX(Component.CENTER_ALIGNMENT);
         this.add(Box.createRigidArea(new Dimension(900, 250)));
         this.add(endText1);
 
-        JLabel endText2 = new JLabel("Zdecyduj dalej co chcesz zrobic ze swoim życiem");
-        endText2.setForeground(new Color(236, 168, 60));
-        endText2.setFont(new Font("ButtonFont", Font.BOLD, 25));
-        endText2.setAlignmentX(Component.CENTER_ALIGNMENT);
+//        JLabel endText2 = new JLabel("Zdecyduj dalej co chcesz zrobic ze swoim życiem");
+//        endText2.setForeground(new Color(236, 168, 60));
+//        endText2.setFont(new Font("Verdana", Font.BOLD | Font.ITALIC, 20));
+//        endText2.setAlignmentX(Component.CENTER_ALIGNMENT);
         this.add(Box.createRigidArea(new Dimension(900,50)));
-        this.add(endText2);
-        this.add(Box.createRigidArea(new Dimension(900, 200)));
+        //this.add(endText2);
+        this.add(Box.createRigidArea(new Dimension(900, 100)));
         this.add(newGameButton());
     }
 
     public JButton newGameButton(){
-        JButton newGameButton = new JButton("Wracam do Kostki");
-        newGameButton.setBackground(new Color(136, 93, 44));
+        JButton newGameButton = new JButton("Wróć do menu głównego");
+        newGameButton.setForeground(Color.getHSBColor(0,1,0.5f));
+        newGameButton.setBackground(Color.BLACK);
         newGameButton.setAlignmentX(Component.CENTER_ALIGNMENT);
-        newGameButton.setFont(new Font("ButtonFont", Font.BOLD, 15));
+        newGameButton.setFont(new Font("ButtonFont", Font.BOLD, 20));
+
+        newGameButton.setOpaque(false);
+        newGameButton.setFocusable(false);
+        newGameButton.setBorderPainted(false);
+
+        newGameButton.setMinimumSize(new Dimension(500, 60));
+        newGameButton.setMaximumSize(new Dimension(500, 60));
+        newGameButton.setPreferredSize(new Dimension(500, 60));
         newGameButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
